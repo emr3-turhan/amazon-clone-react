f2b93049e4d22ad5ec29db936b5ea3455dfccc61
emr3-turhan <osemturhan@gmail.com>
2023-05-30 00:33:22 +0300

    small-fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3s07d8kASbBi-pQF1QgnALOF8Nhz8ZF8",
-  authDomain: "challenge-eddf8.firebaseapp.com",
-  projectId: "challenge-eddf8",
-  storageBucket: "challenge-eddf8.appspot.com",
-  messagingSenderId: "11405769534",
-  appId: "1:11405769534:web:2f207e61fef694baa90a6f",
-  measurementId: "G-V46T4PWMMM",
+  apiKey: "n",
+  authDomain: "a",
+  projectId: "h",
+  storageBucket: "v",
+  messagingSenderId: "r",
+  appId: "r",
+  measurementId: "m",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
